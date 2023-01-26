821674b87001b751117ee698ea720904b4ee1699
gontatata <genedoor2014@yahoo.co.jp>
2023-01-26 23:13:58 +0900

    100%

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,12 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+    def new
+        @book = Book.new
+        @book.year =2019
+    end
 end
