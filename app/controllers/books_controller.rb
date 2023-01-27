2c15e10495f8fc9151a763c02c4a4906175cb266
gontatata <genedoor2014@yahoo.co.jp>
2023-01-27 23:28:10 +0900

    101

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,36 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end
+
     def new
         @book = Book.new
         @book.year =2019
+    end
+    def create
+        book_params=params.require(:book).permit(:year, :month, :inout, :category, :amount)
+        @book = Book.new(book_params)
+        if @book.save
+            flash[:notice] = "家計簿にデータを1件登録しました"
+            redirect_to books_path
+        else
+            render :new
+        end
+    end
+    def edit
+        @book = Book.find(params[:id])
+    end
+    def update
+        @book = Book.find(params[:id])
+        book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+        if @book.update(book_params)
+            redirect_to books_path
+        else
+            render :edit
+        end
+    end
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "/books",to: "books#index"
-  get "/books/new",to: "books#show",as:"new_book"
+  post "/books",to: "books#create"
+  get "/books/new",to: "books#new",as:"new_book"
+  get "/books/:id/edit",to: "books#edit",as:"edit_book"
   get "/books/:id",to: "books#show",as:"book"
+  patch "/books/:id",to: "books#update"
+  delete "/books/:id",to: "books#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
